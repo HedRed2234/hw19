6d622ebf36b63eead5176081f8f1e9ecd8e588b4
Fedir <sabaevevgenij42@gmail.com>
2026-05-15 21:00:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$("#searchBtn").on("click",function(){
+  let capital=$("#capitalInput").val();
+  $("#result").html("");
+  if(capital===""){
+    $("#result").html(
+      "<p>Будь ласка, введіть столицю яку шукаєте</p>"
+    );
+    return;
+  }
+
+  $.ajax({
+    url:`https://restcountries.com/v3.1/capital/${capital}`,
+    method:"GET",
+    success:function(data){
+      let country=data[0];
+      let flag=country.flags.png;
+
+      let description=country.flags.alt;
+      $("#result").html(`
+        <img class="flag" src="${flag}" alt="flag">
+        <p class="description">${description}</p>
+      `);
+    },
+
+    error:function(){
+      $("#result").html(
+        "<p>Будь ласка, введіть столицю яку шукаєте</p>"
+      );
+    }
+  });
+});
